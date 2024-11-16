7c0dae3666ded75578288310e444e9b3f36d757c
sanyam <sanyamkapoor001@gmail.com>
2024-11-16 10:36:29 +0530

    Insertion using spruing jdbc template

--- src/main/java/com/MavenSpringJdbc/Main.java
+++ src/main/java/com/MavenSpringJdbc/Main.java
@@ -1,7 +1,18 @@
 package com.MavenSpringJdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("Config.xml");
+        JdbcTemplate template = (JdbcTemplate) context.getBean("jdbcTemplate");
+
+        String query = "INSERT INTO INFORMATION(firstname, lastname, email, age) value(?, ?, ?, ?)";
+        int result = template.update(query,"Sanyam","Kapoor","sanyamkapoor001@gmail.com","23");
+        System.out.println("No of rows inserted: "+result);
+
     }
 }
